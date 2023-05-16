cfd89913e5f224da3bea956c7a66994b8c0ef868
shiviishiv7 <shiviishiv7@gmail.com>
2023-05-16 14:47:56 +0530

    first commit

---
+++ src/main/java/com/example/cicd/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping
+    public Map<String,String> helloWorld(){
+        Map<String,String> map = new HashMap<>();
+        map.put("shiv","Hello Wolrd");
+        return map;
+    }
+
+}
